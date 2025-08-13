d51283be7ace7630fff049f9a2b279c0df23a882
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-08-13 18:03:08 -0300

    feat(menu): add menu de interatividade para o usuário

---
+++ core/menu.py
@@ -0,0 +1,7 @@
+def menu():
+    print('\n---- CONTROLE DE DESPESA ----\n')
+    print('[1] - Registrar despesa')
+    print('[2] - Listar despesas')
+    print('[3] - Relatório')
+    print('[4] - Remover despesa')
+    print('[0] - Sair')
